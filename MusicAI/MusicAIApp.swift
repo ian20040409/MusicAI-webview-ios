aebd1d15910732832df1e808c7215cc4a2f91858
You <78723629+ian20040409@users.noreply.github.com>
2025-05-21 21:48:49 +0800

    Initial Commit

---
+++ MusicAI/MusicAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAIApp.swift
+//  MusicAI
+//
+//  Created by Ian on 2025/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicAITests/MusicAITests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAITests.swift
+//  MusicAITests
+//
+//  Created by Ian on 2025/5/21.
+//
+
+import Testing
+@testable import MusicAI
+
+struct MusicAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
