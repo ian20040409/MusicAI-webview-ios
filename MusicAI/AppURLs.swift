911afeb652f0b613c40177be16e4031cec4ac892
LNU <78723629+ian20040409@users.noreply.github.com>
2025-11-06 11:09:48 +0800

    Update AppURLs.swift

--- MusicAI/AppURLs.swift
+++ MusicAI/AppURLs.swift
@@ -4,7 +4,7 @@ public enum AppURLs {
     // Set your default/home URL here
     public static let home: URL = {
         // Replace with your desired URL string
-        let string = "https://vercel-redirect-peach.vercel.app/"
+        let string = "https://linyounttu.dpdns.org/"
         guard let url = URL(string: string) else {
             preconditionFailure("Invalid URL: \(string)")
         }
